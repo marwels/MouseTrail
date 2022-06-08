914d6c13418643ae42af533e0997b5dd278a058e
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-06-08 13:41:42 +0200

    add interval for trail()

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-function trail(event) {
+setInterval(function trail(event) {
 
     let posX = event.clientX;
     let posY = event.clientY;
-    let location = `${posX}, ${posY}`;
-    console.log(location);
-}
+    let mouseLocation = `${posX}, ${posY}`;
+    console.log(mouseLocation);
+}, 2)
+
+
 
 window.addEventListener("mousemove", trail);
