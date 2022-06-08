f59d9549a670ce80921c5cbbd422ade40254910a
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-06-08 14:17:18 +0200

    add dot div and style it

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-setInterval(function trail(event) {
+function trail(event) {
 
     let posX = event.clientX;
     let posY = event.clientY;
     let mouseLocation = `${posX}, ${posY}`;
     console.log(mouseLocation);
-}, 2)
+
+
+    let dot = document.createElement("div");
+    document.getElementById("body").appendChild(dot);
+    dot.style.backgroundColor = "rgb(" + Math.random(0, 256) + "," + Math.random(0, 256) + "," + Math.random(0, 256) + ");";
+    let randomForSize = Math.random(10, 30);
+    dot.style.height = randomForSize + "px";
+    dot.style.width = randomForSize + "px";
+    let range = 20;
+    dot.style.left = Math.random(posX - range - randomForSize, posX + range) + "px;";
+    dot.style.top = Math.random(posY - range - randomForSize, posY + range) + "px;";
+}
+
 
 
 
