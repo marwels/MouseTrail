916478865ac3d2b5e7d71fb49796bd542f541c26
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-06-08 12:58:02 +0200

    add location of cursor

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function trail() {
+function trail(event) {
 
+    let posX = event.clientX;
+    let posY = event.clientY;
+    let location = `${posX}, ${posY}`;
+    console.log(location);
 }
 
 window.addEventListener("mousemove", trail);
